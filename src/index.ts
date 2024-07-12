b48e3ad38aac86f1d023ac92306bd1e7686a418e
Kyrua <ang02aezs@gmail.com>
2024-07-11 23:59:50 -0600

    product-controller added

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 import express from 'express';
 import { router } from './routes.index';
+import { db } from './db-connection';
 
 const app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const initApp = async () => {
 
-app.get('/', function(req, res) {
-     res.send(' Hello World');
-})
+  try {
+    
+    await db.authenticate()
+    console.log('Connection to the database has been established successfully.');
 
-app.use('/', router)
+    await db.sync({ force: false });
+    console.log('All models were synchronized successfully.');
+
+    app.use(express.json());
+    
+    app.get('/', function(req, res) {
+      res.send(' Hello World');
+    })
+    
+    app.use('/', router)
+    
+    app.listen(3000, function () {
+      console.log('App listening on port 3000!');
+    });
+
+  }
+  catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+initApp();
+
 
 export default app;
