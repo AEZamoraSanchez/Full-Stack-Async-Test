1875c389e66befbf5c975d111ed3d4e2a1174d64
Kyrua <ang02aezs@gmail.com>
2024-07-15 20:37:44 -0600

    dialect Module added

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,15 +1,20 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config'
-if(process.env.DATABASE_NAME){
-     
-}
-export const db = new Sequelize(process.env.DATABASE_URL!, {
+import pg from 'pg'
+
+
+export const db = new Sequelize({
+     username: process.env.DATABASE_USER,
+     password: process.env.DATABASE_PASSWORD,
+     database: process.env.DATABASE_NAME,
+     host: process.env.DATABASE_HOST,
+     port: +process.env.DATABASE_PORT,
      dialect: 'postgres',
-     dialectOptions: {
-          ssl: {
-              require: true,
-              rejectUnauthorized: false
-          }
-     }
+     dialectModule: pg,
+     define: {
+          charset: 'utf8mb4',
+          collate: 'utf8mb4_general_ci',
+        },
+     logging: false
 });
 
